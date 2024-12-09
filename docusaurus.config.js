ba162f2bc846f4112ab938bff3c051609c5ae053
--get <minsiansu@gmail.com>
2024-12-09 17:06:41 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,15 +109,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -137,10 +128,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
