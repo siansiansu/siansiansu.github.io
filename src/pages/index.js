b3c01aee2b565c2bf44e5cfef4dc727a64f64bbc
--get <minsiansu@gmail.com>
2024-12-10 11:21:01 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export function HomepageHeader() {
     <div className={styles.profileContainer}>
       <div className={styles.imageWrapper}>
         <img
-          src="@site/static/img/profile.jpg"
+          src="/img/profile.jpg"
           alt="Profile"
           className={styles.profileImage}
         />
